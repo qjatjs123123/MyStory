0feb2c6902a3fe33ec493484076639730b07c17e
Hong Boem Sun <qjatjs123123@naver.com>
2023-08-20 01:19:18 +0900

    Bcrypt 로그인 구현

--- routes/memberRouter.js
+++ routes/memberRouter.js
@@ -114,13 +114,18 @@ router.post("/login", (req, res) => {
     const {userId, userPw} = req.body;
     let params = [userId, userPw];
     getConnection((conn) => {
-        const sql = "SELECT * FROM member WHERE userID = ? AND userPassword = ?";
-        let params = [userId, userPw];
+        const sql = "SELECT * FROM member WHERE userID = ?";
+        let params = [userId];
         conn.query(sql,params,
             (err,rows,fields) => {
                 conn.release();
-                console.log(rows);
-                res.send(rows);
+                let flg = false;
+                rows.forEach((row) => {    
+                    console.log();  
+                    if(bcrypt.compareSync(userPw,row.userPassword)){res.send(row); flg=true;}
+                })
+                
+                if (!flg) res.send([]);
             })
     })
 })
